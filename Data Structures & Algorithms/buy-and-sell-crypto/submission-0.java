de97437038758564bffa60d5a834da2cb68fa954
hemanthjangam <166704658+hemanthjangam@users.noreply.github.com>
2026-06-14 21:27:12 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            min = Math.min(price, min);
+            max = Math.max(price - min, max);
+        }
+
+        return max;
+    }
+}
